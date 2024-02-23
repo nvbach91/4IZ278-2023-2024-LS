efa92053d786e30711dcf628be1a6df348b04e11
David Malášek <60174393+davidmalasek@users.noreply.github.com>
2024-02-23 14:34:19 +0100

    mald16-cv02
    Odevzdávám cv02. Odkaz na ESO: https://eso.vse.cz/~mald16/cv02/

---
+++ www/mald16/cv02/index.php
@@ -0,0 +1,8 @@
+
+<?php include "./utilities/utils.php";?>
+<?php require "./classes/Person.php";?>
+<?php include "./includes/head.php";?>
+<main>
+    <?php require "./components/business-cards.php";?>
+</main>
+<?php include "./includes/foot.php";?>
