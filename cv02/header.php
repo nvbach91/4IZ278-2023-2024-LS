08613df17ebe6d6722a880df9adab67aa880853d
farmaextra1 <78812303+farmaextra1@users.noreply.github.com>
2024-02-23 11:49:28 +0100

    lors01-cv02
    Odevzdávám cvičení cv02
    Odkaz: https://eso.vse.cz/~lors01/www/lors01/cv02/

---
+++ cv02/header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="cs">
+<head>
+    <meta charset="UTF-8">
+    <title>Vizitka</title>
+    <link rel="stylesheet" href="style.css"> <!-- Předpokládá, že máte styly v samostatném souboru -->
+</head>
+<body>
