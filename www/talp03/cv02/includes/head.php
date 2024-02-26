3d17932b2e8849df249ce62c5e14bf1192c9e6c5
Patrik <139178681+AsemikTEA@users.noreply.github.com>
2024-02-26 13:09:11 +0100

    talp03-cv02
    Odevzdávám práci ze cvičení 2
    
    https://eso.vse.cz/~talp03/cv02/

---
+++ www/talp03/cv02/includes/head.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cards</title>
+    <link rel="stylesheet" href="./css/style.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
