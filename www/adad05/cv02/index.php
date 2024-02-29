1d13a1be05ce8e81a41df4e4a96f9d3d7188d1f2
danieladam2001 <138229177+danieladam2001@users.noreply.github.com>
2024-02-29 22:33:42 +0100

    adad05-cv02
    Odkaz na server eso: https://eso.vse.cz/~adad05/cv02/

---
+++ www/adad05/cv02/index.php
@@ -0,0 +1,6 @@
+<?php include './includes/head.php'; //import statické části html souboru 
+?>
+<?php require './components/main.php'; //import části, která obsahuje PHP, nutno použít require místo include 
+?>
+<?php include './includes/footer.php'; ?>
+
