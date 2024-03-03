0544b28627c5849b79b4ec37a8e89631583f0400
Vilém Charwot <mrlogen1299@gmail.com>
2024-03-03 14:58:55 +0100

    homework #3 submission

---
+++ www/chav07/cv03/includes/top.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Inter">
+    <link href="./main.css" rel="stylesheet" type="text/css"/>
+    <link rel="icon" href="./favicon.ico">
+    <title>Card Form</title>
+</head>
+<body>
+    <main class="main-wrapper">
