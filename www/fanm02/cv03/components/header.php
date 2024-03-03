a58d713633e7982cfb26fce36d433f9884d793e4
martin1080p <44344165+martin1080p@users.noreply.github.com>
2024-03-03 21:23:10 +0100

    fanm02-cv03
    https://eso.vse.cz/~fanm02/cv03/index.php

---
+++ www/fanm02/cv03/components/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Registration Form</title>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="styles/style.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <header>
+        <h1 class="title">Registration Form</h1>
+    </header>
+    <body>
