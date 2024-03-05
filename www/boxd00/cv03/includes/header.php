d5a3d0b5052083adb6a10f9739c4d508ca0080ce
boxandav <113298852+boxandav@users.noreply.github.com>
2024-03-05 11:13:51 +0100

    boxd00-cv03
    Odevzdávám cvičení 3
    https://eso.vse.cz/~boxd00/cv03

---
+++ www/boxd00/cv03/includes/header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <link rel="stylesheet" type="text/css" href="./static/style.css">
+        <meta charset="UTF-8">
+        <title>Registration</title>
+    </head>
+    <body>
