6a500636e08837cc9204bbac2868e09baa54fd09
Nguyen Viet Bach <nvbach91@outlook.com>
2024-03-12 22:41:38 +0100

    Delete cv02 directory

--- cv02/header.php
+++
@@ -1,8 +0,0 @@
-<!DOCTYPE html>
-<html lang="cs">
-<head>
-    <meta charset="UTF-8">
-    <title>Vizitka</title>
-    <link rel="stylesheet" href="style.css"> <!-- Předpokládá, že máte styly v samostatném souboru -->
-</head>
-<body>
