ba3ab17f4ba6727fc8458449ac0628dc47706044
KubaDavid <jakub.david@resonect.cz>
2024-03-14 21:27:03 +0100

    davj05 oprava

--- www/davj05/cv03/index.php
+++ www/davj05/cv03/index.php
@@ -1,47 +1,107 @@
 <?php
 $base_dir = "/cv03";
-$view_dir = "/views/";
-$components_dir = "/components/";
+$view_dir = "./views/";
+$components_dir = "./components/";
+$pic = "";
 
-// Get the route from the request and check if it is htmx request
-$route = str_replace($base_dir, '', $_SERVER['REQUEST_URI']);
-$hx_request = isset($_SERVER['HX_REQUEST']) && $_SERVER['HX_REQUEST'] == 'true';
-// var_dump($_SERVER);
-// Function will either generate full page or just components for htmx
-register_shutdown_function(function () {
-    global $content, $hx_request, $view_dir;
-    if ($hx_request) {
-        echo $content;
+if (!empty($_POST)) {
+    $name = htmlspecialchars(trim($_POST["name"]));
+    $gender = htmlspecialchars(trim($_POST["gender"]));
+    $email = htmlspecialchars(trim($_POST["email"]));
+    $phone = htmlspecialchars(trim($_POST["phone"]));
+    if (isset($_POST["pic"])) {
+        $pic = htmlspecialchars(trim($_POST["pic"]));
     } else {
-        require __DIR__ . $view_dir . "_layout.php";
+        $pic = "";
     }
-});
+    $deck_name = htmlspecialchars(trim($_POST["deck_name"]));
+    $deck_count = htmlspecialchars(trim($_POST["deck_count"]));
+    $errors = [];
 
-echo $route;
+    // Regex pattern for Czech phone numbers
+    $pattern = "/(\\+420|00420)?\\s?(\\d{3})\\s?(\\d{3})\\s?(\\d{3})/";
 
-// Ob start to capture output in one buffer
-// Then we can decide if we want to send full page or just components
-ob_start();
-switch ($route) {
-    case "":
-    case "/":
-        require __DIR__ . $view_dir . "home.php";
-        break;
+    if (! preg_match($pattern, $phone)) {
+        array_push($errors, "Prosím zadejte platné telefonní číslo");
+    }
+    if (strlen($name) < 3) {
+        array_push($errors, "Vase jmeno musi mit vice jak 3 znaky");
+    }
+    if (! filter_var($pic, FILTER_VALIDATE_URL)) {
+        array_push($errors, "Použijte prosím platnou URL obrázku");
+    }
+    if (strlen($deck_name) < 3) {
+        array_push($errors, "Váš název balíčku musí mít více než 3 znaky");
+    }
+    if (! filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        array_push($errors, "Email není validní");
+    }
 
-    case "/htmx":
-        require __DIR__ . $components_dir . "fragments.php";
-        break;
-
-    default:
-        http_response_code(404);
+    // All good
+    if (count($errors) == 0) {
+        $successMessage = "Děkuji za registraci";
+    }
 }
-$content = ob_get_clean();
-
-
 
 ?>
 
+<?php require $view_dir . "head.php"; ?>
 
 
+<body>
+    <div class="container mx-auto md:px-20 py-10">
+        
+    <form method="POST" action="<?php echo $_SERVER["PHP_SELF"] ?>">
+    <h1>Formlář</h1>
+    <?php if (! empty($errors)): ?>
+            <?php foreach($errors as $error): ?>
+                <div role="alert" class="alert alert-error">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+                <p><?php echo $error;?></p>
+                </div> 
+            <?php endforeach;?>       
+    <?php endif;?>
+    <div class="m-2 p-2">
+        <label>Jméno</label>
+        <input class="text" name="name" placeholder="Jméno" value="<?php echo isset($name) ? $name : "" ?>">
+    </div>
+    <div class="m-2 p-2">
+        <label>Pohlaví</label>
+        <select class="form-select" name="gender">
+            <option <?php echo isset($gender) && $gender == "female" ? "selected" : ""; ?> value="female">Žena</option>
+            <option <?php echo isset($gender) && $gender == "male" ? "selected" : ""; ?> value="male">Muž</option>
+        </select>
+    </div>
+    <div class="m-2 p-2">
+        <label>Email</label>
+        <input class="text" name="email" placeholder="" value="<?php echo isset($email) ? $email : "" ?>">
+    </div>
+    <div class="m-2 p-2">
+        <label>Telefon</label>
+        <input class="text" name="phone" placeholder="+420 888 888 888" value="<?php echo isset($phone) ? $phone : "" ?>">
+    </div>
+    <div class="m-2 p-2">
+        <label>URL Obrázku</label>
+        <input class="text" name="pic" placeholder="" value="<?php echo isset($pic) ? $pic : "" ?>">
+    </div>
+    <div class="m-2 p-2">
+        <label>Jméno balíčku</label>
+        <input class="text" name="deck_name" placeholder="" value="<?php echo isset($deck_name) ? $deck_name: "" ?>">
+    </div>
+    <div class="m-2 p-2">
+        <label>Velikost balíčku</label>
+        <input class="text" name="deck_count" placeholder="" value="<?php echo isset($deck_count) ? $deck_count : "" ?>">
+    </div>
+    <button class="btn btn-success" type="submit">
+        <p>Odeslat</p>
+    </button>
+</form>
+               
 
 
+        
+    </div>
+</body>
+
+
+<?php require $view_dir . "foot.php"; ?>
