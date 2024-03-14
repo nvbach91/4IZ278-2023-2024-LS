660e102b4715f9086f85cb65a4ae3158da0472b3
Jakub David <jakub-david@outlook.com>
2024-03-14 23:29:15 +0100

    davj05-cv04

---
+++ www/davj05/cv04/admin/users.php
@@ -0,0 +1,31 @@
+<?php
+
+require __DIR__ . "../functions.php";
+
+$users = fetch_users("/users.db");
+?>
+
+
+<?php require $view_dir . "head.php"; ?>
+
+<body>
+
+
+<div>
+        <ul class="users">
+            <?php foreach ($users as $user_email => $user_data): ?>
+            <li class="user">
+                <div class="name"><?php echo $user_data["name"]; ?></div>
+                <div class="email"><?php echo $user_email; ?></div>
+            </li>
+            <?php endforeach; ?>
+        </ul>
+    </div>
+               
+
+
+        
+    </div>
+</body>
+
+<?php require $view_dir . "foot.php"; ?>
