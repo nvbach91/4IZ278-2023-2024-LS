8bf8133f9d903860e5f16ca935bbe507d6d3a772
David Malášek <60174393+davidmalasek@users.noreply.github.com>
2024-03-20 16:21:20 +0100

    mald16-cv05
    https://eso.vse.cz/~mald16/cv05/

---
+++ www/mald16/cv05/includes/head.php
@@ -0,0 +1,17 @@
+<?php require "./database.php";
+$players = new PlayersDB();
+$teams = new TeamsDB();
+$matches = new MatchesDB();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
---
+++ www/mald16/cv05/method-tests.php
@@ -0,0 +1,23 @@
+<h1>cv05</h1>
+<br>
+<h2>PlayersDB</h2>
+<div><?php $players->create("") ?></div>
+<div><?php $players->find("") ?></div>
+<div><?php $players->update("", "") ?></div>
+<div><?php $players->delete("") ?></div>
+
+<br>
+
+<h2>PlayersDB</h2>
+<div><?php $teams->create("") ?></div>
+<div><?php $teams->find("") ?></div>
+<div><?php $teams->update("", "") ?></div>
+<div><?php $teams->delete("") ?></div>
+
+<br>
+
+<h2>PlayersDB</h2>
+<div><?php $matches->create("") ?></div>
+<div><?php $matches->find("") ?></div>
+<div><?php $matches->update("", "") ?></div>
+<div><?php $matches->delete("") ?></div>
