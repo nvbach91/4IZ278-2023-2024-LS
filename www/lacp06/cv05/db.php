f38a7b84a610f48df050ab513f54a4c091abf0a7
Pavel Lacina <157717103+ParzivalPavlis@users.noreply.github.com>
2024-03-22 11:16:45 +0100

    lacp06-cv05-oprava
    Oprava cv05

--- www/lacp06/cv05/db.php
+++ www/lacp06/cv05/db.php
@@ -36,6 +36,6 @@ abstract class Database implements DatabaseOperations
   protected $pdo;
   public function __construct()
   {
-    $pdo = DatabaseConnection::getPDOConnection();
+    $this->pdo = DatabaseConnection::getPDOConnection();
   }
 }
