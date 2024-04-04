7322ee9db5884e9ba1a1df9cff167f4e3cb5e6d4
jana19-vse <160042128+jana19-vse@users.noreply.github.com>
2024-04-04 23:13:45 +0200

    výmaz hesla do DB v gitu

--- jana19/cv05/constants.php
+++ jana19/cv05/constants.php
@@ -6,7 +6,7 @@ const DB_DATABASE = 'starwars';
 //const DB_DATABASE = 'jana19';
 const DB_USERNAME = 'jana19'; // na eso bude zde xname
 // const DB_USERNAME = 'root'; 
-const DB_PASSWORD = 'yohThietae3joochae';  // heslo do DB
+const DB_PASSWORD = '';  // heslo do DB
 // const DB_PASSWORD = '';  // heslo do DB
 
 ?>
