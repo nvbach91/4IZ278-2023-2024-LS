d90e3e225a53a647809629c43b3800fdc6af6c2a
Le Anh Viet Linh <lav.linh@gmail.com>
2024-04-04 19:31:31 +0200

    lean04_cv06

---
+++ www/lean04/cv06/database/ProductsDB.php
@@ -0,0 +1,29 @@
+<?php
+
+require_once "Database.php";
+
+class ProductsDB extends Database
+{
+    public function create($data)
+    {
+    }
+    public function find($query)
+    {
+    }
+    public function findAll()
+    {
+        return $this->runQuery('SELECT * FROM cv06_products;', []);
+    }
+    public function findByCategory($query)
+    {
+        return $this->runQuery('SELECT * FROM cv06_products WHERE category_id = :category_id;', [
+            'category_id' => $query['category_id']
+        ]);
+    }
+    public function update($query, $data)
+    {
+    }
+    public function delete($query)
+    {
+    }
+}
