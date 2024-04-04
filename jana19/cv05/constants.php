711adee27ba39c4678ead430167015cfb2f4bcc2
jana19-vse <160042128+jana19-vse@users.noreply.github.com>
2024-04-04 23:20:32 +0200

    výmaz username do DB (pro jistotu)

--- jana19/cv05/constants.php
+++ jana19/cv05/constants.php
@@ -4,7 +4,7 @@
 const DB_HOSTNAME = 'localhost';
 const DB_DATABASE = 'starwars';
 //const DB_DATABASE = 'jana19';
-const DB_USERNAME = 'jana19'; // na eso bude zde xname
+const DB_USERNAME = ''; // na eso bude zde xname
 // const DB_USERNAME = 'root'; 
 const DB_PASSWORD = '';  // heslo do DB
 // const DB_PASSWORD = '';  // heslo do DB
