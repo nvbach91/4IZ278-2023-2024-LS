caba1d7af1461d9cd47620565b3069c8b3775a2c
jakubsrnka <48597670+jakubsrnka@users.noreply.github.com>
2024-04-05 11:49:01 +0200

    srnj01: cv06

---
+++ www/srnj01/cv06/components/sidebar.php
@@ -0,0 +1,7 @@
+<h1 class="my-4">Shop Name</h1>
+<div class="list-group">
+  <a class="list-group-item" href="?ctg=0">All Categories</a>
+  <a class="list-group-item" href="?ctg=1">Category 1</a>
+  <a class="list-group-item" href="?ctg=2">Category 2</a>
+  <a class="list-group-item" href="?ctg=3">Category 3</a>
+</div>
