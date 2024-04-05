609025ab670877569bb5e995e17ca39dce24a314
jakubsrnka <48597670+jakubsrnka@users.noreply.github.com>
2024-04-05 11:51:45 +0200

    srnj01: cv06: sidebar is loaded from DB

--- www/srnj01/cv06/components/sidebar.php
+++ www/srnj01/cv06/components/sidebar.php
@@ -1,7 +1,15 @@
+<?php
+
+require_once('db/eshop.php');
+$db = new CategoriesDB();
+$categories = $db->find();
+
+?>
+
 <h1 class="my-4">Shop Name</h1>
 <div class="list-group">
   <a class="list-group-item" href="?ctg=0">All Categories</a>
-  <a class="list-group-item" href="?ctg=1">Category 1</a>
-  <a class="list-group-item" href="?ctg=2">Category 2</a>
-  <a class="list-group-item" href="?ctg=3">Category 3</a>
+  <?php foreach ($categories as $category) { ?>
+    <a class="list-group-item" href="?ctg=<?php echo $category['id']; ?>"><?php echo $category['name']; ?></a>
+  <?php } ?>
 </div>
