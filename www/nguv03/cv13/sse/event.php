acf4f14acb828cb6839eb0bed89fbbda522ca2fe
Viet Bach Nguyen <nvbach91@outlook.com>
2024-04-07 22:11:49 +0200

    added server sent event example

--- www/nguv03/cv13/sse/event.php
+++ www/nguv03/cv13/sse/event.php
@@ -1,9 +1,25 @@
 <?php
-header('Content-Type: text/event-stream');
-header('Cache-Control: no-cache');
+header("Cache-Control: no-cache");
+header("Content-Type: text/event-stream");
 
-$time = date('r');
-echo "data: {$time}\n\n";
-flush();
+while (true) {
+  $counter = rand(1, 10);
+  $time = date('H:m:s');
+  $eventType = $counter % 2 == 0 ? 'counter' : 'time';
+  $data = $eventType == 'counter' ? $counter : $time;
+  echo "event: $eventType\n";
+  echo "data: $data\n\n";
+  if (ob_get_length() > 0) {
+    ob_end_flush();
+  }
+  flush();
+
+  // Break the loop if the client aborted the connection (closed the page)
+  if (connection_aborted()) {
+    exit();
+  }
+  sleep(1);
+}
+
 ?>
 
