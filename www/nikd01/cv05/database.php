240bdd8a547bef8e4ba189b0f8641267514ebd02
Dmitrii Nikolaev <dmitrynikolaev08@gmail.com>
2024-05-12 19:09:52 +0200

    nikd01-sp / initial set up (Laravel Breeze + React)

---
+++ www/nikd01/cv05/database.php
@@ -0,0 +1,63 @@
+<?php
+
+require_once 'db.php';
+
+class PlayersDB extends Database
+{
+    public function create($data)
+    {
+        echo "PlayersDB create";
+    }
+    public function find($query)
+    {
+        echo "PlayersDB find";
+    }
+    public function update($query, $data)
+    {
+        echo "PlayersDB update";
+    }
+    public function delete($query)
+    {
+        echo "PlayersDB delete";
+    }
+}
+class TeamsDB extends Database
+{
+    public function create($data)
+    {
+        echo "TeamsDB create";
+    }
+    public function find($query)
+    {
+        echo "TeamsDB find";
+    }
+    public function update($query, $data)
+    {
+        echo "TeamsDB update";
+    }
+    public function delete($query)
+    {
+        echo "TeamsDB delete";
+    }
+}
+class MatchesDB extends Database
+{
+    public function create($data)
+    {
+        echo "MatchesDB create";
+    }
+    public function find($query)
+    {
+        echo "MatchesDB find";
+    }
+    public function update($query, $data)
+    {
+        echo "MatchesDB update";
+    }
+    public function delete($query)
+    {
+        echo "MatchesDB delete";
+    }
+}
+
+//$pdoConnection = DatabaseConnection::getPDOConnection();
---
+++ www/nikd01/sp/resources/js/Components/LinkButton.jsx
@@ -0,0 +1,9 @@
+import {Link} from "@inertiajs/react";
+
+export default function LinkButton({ className = '', children, ...props }) {
+    return (
+        <Link {...props} className={`inline-flex items-center px-4 py-2 bg-blue-700 border border-transparent rounded-md font-semibold text-white hover:bg-blue-800 focus:bg-blue-900 active:bg-blue-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 duration-300 transition-colors ${className}`}>
+            {children}
+        </Link>
+    )
+}
