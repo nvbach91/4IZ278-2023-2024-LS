56c9da053d84a105c66de46baf8f1780ce036fd4
Vilém Charwot <mrlogen1299@gmail.com>
2024-06-03 00:07:23 +0200

    comments for individul values

--- www/chav07/chav07-sp/src/config.php
+++ www/chav07/chav07-sp/src/config.php
@@ -1,10 +1,20 @@
 <?php
 
-define("BASE_URL", "/www/BookBookGo/src");
+
+//BASE_URL has to be changed to reflect your enviroment setup
+define("BASE_URL", "/chav07-sp/src");
+
+//ITEMS_PER_PAGE is meant for pagination
 define("ITEMS_PER_PAGE", 20);
+
+//APP_ID is facebook's app id for OAUTH2
 define("APP_ID", 0);
+//APP_SECRET is facebook's app secret for OAUTH2
 define("APP_SECRET", "{secret}");
+
+//APP_REDIRECT has to be changed to reflect your enviroment setup. Change the http://localhost to your domain (e.g. http://example.com )
 define("APP_REDIRECT", "http://localhost". BASE_URL. "/authentication/oauth.php");
+//GRAPH_API_VERSION is current facebooks Graph api version
 define("GRAPH_API_VERSION","v20.0");
 
 
