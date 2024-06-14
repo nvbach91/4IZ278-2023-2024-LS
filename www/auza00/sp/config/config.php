b081d2555fe5b33258d87f90366177e6fb035f47
auzkya <91533800+auzkya@users.noreply.github.com>
2024-06-14 13:37:00 +0200

    Update config.php

--- www/auza00/sp/config/config.php
+++ www/auza00/sp/config/config.php
@@ -1,6 +1,6 @@
 <?php
-    const HOST_NAME = 'md405.wedos.net';
-    const DB_NAME = 'd341941_spot';
-    const USER_NAME = 'a341941_spot';
-    const PASSWORD = 'rcMcSkEs';
+    const HOST_NAME = '';
+    const DB_NAME = '';
+    const USER_NAME = '';
+    const PASSWORD = '';
 ?>
